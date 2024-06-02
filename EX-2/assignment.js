37dd6cc0b20ef12fe5a988ed65cffddf46bce12d
OVERGEARED18 <emmanueloladimeji435@gmail.com>
2024-06-02 19:25:53 +0100

    assignment

--- EX-2/assignment.js
+++ EX-2/assignment.js
@@ -32,6 +32,10 @@ if (parsedNumber !== adjustedNumber) {
 }
 
 5// Check if 'on' is found in both python and jargon
+let python = "python"
+let jargon = "a programmer's jargon"
+console.log(python.includes("on"));
+console.log(jargon.includes("on"));
 
 6// I hope this course is not full of jargon. Check if jargon is in the sentence. in java script
 
@@ -45,11 +49,19 @@ if (sentence.includes(searchTerm)) {
 }
 
 7// Generate a random number between 0 and 100 inclusively.
-
+console.log("import random")
+random_number = random.radiant(0, 100);
+console.log(random_number);
 
 8// Generate a random number between 50 and 100 inclusively.
+console.log("import random")
+random_number = random.radiant(50, 100);
+console.log(random_number);
 
 9// Generate a random number between 0 and 255 inclusively.
+console.log("import random")
+random_number = random.radiant(0, 255);
+console.log(random_number);
 
 10// Access the 'JavaScript' string characters using a random number
 
